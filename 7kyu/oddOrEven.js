0be2df5f794a8623222235433acd6b0e729209b4
yaroslavbilokin <48564795+yaroslavbilokin@users.noreply.github.com>
2020-02-12 15:14:41 +0200

    Update oddOrEven.js

--- 7kyu/oddOrEven.js
+++ 7kyu/oddOrEven.js
@@ -20,3 +20,10 @@ function oddOrEven(array) {
         return result = sum % 2 == 0 ? 'even' : 'odd';
     }
 }
+
+
+//
+
+function oddOrEven(arr) {
+  return arr.reduce((a,b)=>a+b,0) % 2 ? 'odd' : 'even';
+}
