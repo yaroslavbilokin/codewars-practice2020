7fcdc1eda37f47250dc541f7d9c61898ab78676b
yaroslavbilokin <48564795+yaroslavbilokin@users.noreply.github.com>
2020-02-21 21:12:51 +0200

    Create xor.js

---
+++ 8kyu/xor.js
@@ -0,0 +1,10 @@
+https://www.codewars.com/kata/56fa3c5ce4d45d2a52001b3c/train/javascript
+
+In some scripting languages like PHP, there exists a logical operator (e.g. &&, ||, and, or, etc.) called the "Exclusive Or" (hence the name of this Kata). The exclusive or evaluates two booleans. It then returns true if exactly one of the two expressions are true, false otherwise. For example:
+
+false xor false == false // since both are false
+true xor false == true // exactly one of the two expressions are true
+false xor true == true // exactly one of the two expressions are true
+true xor true == false // Both are true.  "xor" only returns true if EXACTLY 
+
+const xor = (a,b) => a!= b;
