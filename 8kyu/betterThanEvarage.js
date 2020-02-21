20597db1bbb737f1cf8f06383b0e89653629c332
yaroslavbilokin <48564795+yaroslavbilokin@users.noreply.github.com>
2020-02-21 20:57:36 +0200

    Create betterThanEvarage.js

---
+++ 8kyu/betterThanEvarage.js
@@ -0,0 +1,28 @@
+https://www.codewars.com/kata/5601409514fc93442500010b/train/javascript
+
+function betterThanAverage(classPoints, yourPoints) {
+    const sumPoints = classPoints.reduce((a, b) => a + b);
+    const middlePoint = sumPoints / classPoints.length;
+    if (middlePoint > yourPoints) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+// Short Solution
+
+function betterThanAverage(classPoints, yourPoints) {
+    return yourPoints > classPoints.reduce((a, b) => a + b, 0) / classPoints.length;
+}
+
+
+There was a test in your class and you passed it.Congratulations!
+But you're an ambitious person. You want to know if you're better than the average student in your class.
+
+You receive an array with your peers' test scores. Now calculate the average and compare your score!
+
+Return True if you're better, else False!
+
+Note:
+Your points are not included in the array of your class's points. For calculating the average point you may add your point to the given array!
