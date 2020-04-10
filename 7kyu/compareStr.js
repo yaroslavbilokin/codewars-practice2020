da3532d6db8ec254aed32ebd85fc9af2d51f9c11
yaroslavbilokin <48564795+yaroslavbilokin@users.noreply.github.com>
2020-04-10 21:14:04 +0300

    Create compareStr.js

---
+++ 7kyu/compareStr.js
@@ -0,0 +1,24 @@
+https://www.codewars.com/kata/56cd44e1aa4ac7879200010b/train/javascript
+
+Is the string uppercase?
+Task
+Add the isUpperCase method to String to see whether the string is ALL CAPS. For example:
+
+"c".isUpperCase() == false
+"C".isUpperCase() == true
+"hello I AM DONALD".isUpperCase() == false
+"HELLO I AM DONALD".isUpperCase() == true
+"ACSKLDFJSgSKLDFJSKLDFJ".isUpperCase() == false
+"ACSKLDFJSGSKLDFJSKLDFJ".isUpperCase() == true
+In this Kata, a string is said to be in ALL CAPS whenever it does not contain any lowercase letter so any string containing no letters at all is trivially considered to be in ALL CAPS.
+//
+
+String.prototype.isUpperCase = function() {
+  return this.toUpperCase().includes(this)
+}
+
+//
+
+String.prototype.isUpperCase = function() {
+  return this == this.toUpperCase()
+}
