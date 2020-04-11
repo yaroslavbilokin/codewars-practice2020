499fdb2703a235c23fec1f9e8c5f425d3d506997
yaroslavbilokin <48564795+yaroslavbilokin@users.noreply.github.com>
2020-04-11 23:28:27 +0300

    Create esreveR.js

---
+++ 8kyu/esreveR.js
@@ -0,0 +1,15 @@
+https://www.codewars.com/kata/5413759479ba273f8100003d/train/javascript
+
+Write a function reverse which reverses a list (or in clojure's case, any list-like data structure)
+
+(the dedicated builtin(s) functionalities are deactivated)
+
+//
+
+reverse = function(array) {
+  let reverseArr = [];
+  for (let i = array.length - 1; i >= 0; i--) {
+    reverseArr.push(array[i])
+  }
+  return reverseArr;
+}
